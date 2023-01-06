18d2c857087d4779c9ba37700d9b9d6d7d18f3fd
admin <admin@DELL>
2023-01-06 13:52:24 +0530

    LabActivity8

---
+++ LabActivity8/src/com/question2/StateMain.java
@@ -0,0 +1,19 @@
+package com.question2;
+
+import java.util.Iterator;
+
+public class StateMain {
+
+	public static void main(String[] args) {
+		State a=new State();
+		a.addState("Maharshatra");			//adding sate name
+		a.addState("Panjab");
+		a.addState("Kerla");
+		a.addState("Gujarat");
+		
+		System.out.println("Retrive the details of state:"+a.retriveState("Kerla"));		//Retrieves the specific state name 
+
+	}
+
+}
+
