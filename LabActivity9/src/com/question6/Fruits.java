acd2e3b7d70d86e30ce057e8522be288efb2723f
admin <admin@DELL>
2023-01-20 14:45:58 +0530

    LabActivity9

---
+++ LabActivity9/src/com/question6/Fruits.java
@@ -0,0 +1,30 @@
+package com.question6;
+
+public class Fruits {
+
+public static String[] fruits= {"orange", "apple", "banana", "pear", "cantaloupe"};
+
+public static int found =0;
+        
+    public static void main(String[] args)
+    {
+     
+        
+        for (int i=0; i < fruits.length; i++)
+        {
+            if(fruits[i].equals("banana"))
+            {
+            found++;
+            }
+}
+        if(found > 0)
+        {
+            System.out.println("The word banana is in the array");
+        }
+        else
+        {
+            System.out.println("The word banana is not in the array");
+        }
+}
+}
+
